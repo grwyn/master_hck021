0a31a0d29134ed196ed41ae8e9bf687e298ea848
grwyn <gzulqarnain17@gmail.com>
2024-10-03 22:38:59 +0700

    upload

--- NGC9.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import requests
-
-driver = webdriver.Chrome("chromedriver.exe")
-link = "https://coinmarketcap.com/"
-
-page = requests.get(link)
-print(page)
---
+++ aoi_gerwyn.py
@@ -0,0 +1,5 @@
+from fastapi import FastAPI, HTTPException
+import uvicorn
+
+gerwyn = FastAPI()
+
